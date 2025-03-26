a8a7a6f248de1bb20fedef60012467f3935985dc
Abhishek Kulkarni <kul.abhishake@gmail.com>
2025-03-26 11:44:02 +0530

    config and artifact skeleton created

---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import os
+from networksecurity.constants import training_pipeline
+
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        pass
+
+class DataIngestionConfig:
+    def __init__(self):
+        pass
+
+class DataValidationConfig:
+    def __init__(self):
+        pass
+
+class DataTransformationConfig:
+    def __init__(self):
+        pass
+
+class ModelTrainerConfig:
+    def __init__(self):
+        pass
+
+class ModelEvaluationConfig:
+    def __init__(self):
+        pass
+
+class ModelPusherConfig:
+    def __init__(self):
+        pass
