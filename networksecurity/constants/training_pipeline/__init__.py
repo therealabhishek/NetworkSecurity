27840bc8f1b8136218466ed48f620e27eae3e0e0
Abhishek Kulkarni <kul.abhishake@gmail.com>
2025-03-27 10:02:44 +0530

    ingestion constants updated

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,57 @@
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+# TRAINING PIPELINE CONSTANTS
+
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "NetworkData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+MODEL_FILE_NAME = "model.pkl"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SCHEMA_DROP_COLS = "drop_columns"
+SAVED_MODEL_DIR =os.path.join("saved_models")
+
+
+# DATA INGESTION CONSTANTS
+
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "NetworkSecurity"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+
+
+# DATA VALIDATION CONSTANTS
+
+
+
+
+# DATA TRANSFORMATION CONSTANTS
+
+
+
+
+# MODEL TRAINER CONSTANTS
+
+
+
+
+# MODEL EVALUATION CONSTANTS
+
+
+
+
+# MODEL PUSHER CONSTANTS
